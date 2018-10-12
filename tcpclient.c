316cac6bb988e90c16fddfb59ab5fa7b14f6d2a5
Kasey Ann <kaseystowell@Kaseys-MBP.attlocal.net>
2018-10-11 21:37:46 -0400

    code added

---
+++ tcpclient.c
@@ -0,0 +1,104 @@
+#include <sys/socket.h> // program interface to any network socket (generic)
+#include <netinet/in.h> // specific to internet protocols (tcp, udp, idp)
+#include <arpa/inet.h> // specific to internet protocols (tcp, udp, idp)
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define DELIM "."
+
+int check_port(char*);
+
+int main(int argc, char** argv) {
+	char addr[1024], port[1024];
+	unsigned long ip = 0;
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		printf("There was an error creating the socket\n");
+		return 1;
+	}
+
+	printf("Please give an address: \n");
+	fgets(addr, 1024, stdin);
+	addr[strcspn(addr, "\n")] = 0;
+	if (0 == inet_pton(AF_INET, addr, &ip)) {
+        printf("Failed\n");
+        exit(0);
+    }
+
+	printf("Please give a port number: \n");
+	fgets(port, 1024, stdin);
+	port[strcspn(port, "\n")] = 0;
+	while(!check_port(port)) {
+		printf("Please give a port number: \n");
+		fgets(port, 1024, stdin);
+		port[strcspn(port, "\n")] = 0;
+	}
+	int p = atoi(port);
+
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family=AF_INET;
+	serveraddr.sin_port=htons(p); 
+	serveraddr.sin_addr.s_addr=inet_addr(addr);
+
+	int e = connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	
+	if (e < 0) {
+		printf("There was an error connecting\n");
+		return 2;
+	}
+
+	printf("Client started \n");
+	fflush(stdout);
+
+	fd_set clientfd;
+	FD_ZERO(&clientfd);
+	FD_SET(sockfd, &clientfd);
+	FD_SET(STDIN_FILENO, &clientfd);
+
+	while(1) {
+		fd_set tmp_set = clientfd;
+		select(FD_SETSIZE, &tmp_set, NULL, NULL, NULL);
+		for (int i = 0; i < FD_SETSIZE; i++) {
+			if (FD_ISSET(i, &tmp_set)) {
+				if (i == sockfd) {
+					char line[5000];
+					recv(sockfd, line, 5000, 0);
+					printf("%s\n", line);
+					if (strcmp(line, "exit\n") == 0) {
+						printf("Server shutting down.\n");
+						send(sockfd, line, strlen(line) + 1, 0);
+						close(sockfd);
+						FD_CLR(sockfd, &clientfd);
+						exit(0);
+					}
+				} else if (i == STDIN_FILENO) {
+					char line2[5000];
+					fgets(line2, 5001, stdin);
+					if (strcmp(line2, "exit\n") == 0) {
+						printf("Server shutting down.\n");
+						send(sockfd, line2, strlen(line2) + 1, 0);
+						close(sockfd);
+						FD_CLR(sockfd, &clientfd);
+						exit(0);
+					} else {
+						send(sockfd, line2, strlen(line2) + 1, 0);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int check_port(char* port) {
+	int digits = 0;
+	for (int i = 0; i < sizeof(port); i++) {
+		if ((port[i] != '0') && !atoi(&port[i])) digits++;
+	}
+	if ((digits > 0 && digits < 6) && (atoi(port) > 0 && atoi(port) < 65536)) return 1;
+	else return 0;
+}
---
+++ tcpserver.c
@@ -0,0 +1,110 @@
+#include <sys/socket.h> // program interface to any network socket (generic)
+#include <netinet/in.h> // specific to internet protocols (tcp, udp, idp)
+#include <arpa/inet.h> // specific to internet protocols (tcp, udp, idp)
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int main(int argc, char** argv) {
+	char port[1024];
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// check for error
+	if (sockfd < 0) {
+		printf("Problem creating socket\n");
+		return 1;
+	}
+
+	printf("Please give a port number: \n");
+	fgets(port, 1024, stdin);
+	port[strcspn(port, "\n")] = 0;
+	while(!check_port(port)) {
+		printf("Please give a port number: \n");
+		fgets(port, 1024, stdin);
+		port[strcspn(port, "\n")] = 0;
+	}
+	int p = atoi(port);
+
+	// file descriptor set and set all to zero
+	fd_set sockets;
+	FD_ZERO(&sockets);
+
+	struct sockaddr_in serveraddr, clientaddr;
+	serveraddr.sin_family=AF_INET;
+	serveraddr.sin_port=htons(p);
+	serveraddr.sin_addr.s_addr=INADDR_ANY;
+
+	int b = bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	
+	// error check very important here
+	if (b < 0) {
+		printf("Bind error\n");
+		return 3;
+	}
+
+	// 10 is the backlog for buffer of connection requests that we havent handled.
+	listen(sockfd, 10);
+	printf("Listening on server\n");
+	fflush(stdout);
+
+	// put the socket in file descriptor.
+	FD_SET(sockfd, &sockets);
+	FD_SET(STDIN_FILENO, &sockets);
+
+	// we want the server to keep accepting clients. an infinite loop is most common
+	while(1) {
+		fd_set tmp_set = sockets;
+		select(FD_SETSIZE, &tmp_set, NULL, NULL, NULL);
+		for (int i = 0; i < FD_SETSIZE; i++) {
+			if (FD_ISSET(i, &tmp_set)) {
+				if (i == sockfd) { // get new connection
+					int len = sizeof(clientaddr);
+					int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+					FD_SET(clientsocket, &sockets);
+					printf("Added client \n");
+				}
+				else if (i == STDIN_FILENO) {
+					char line2[5000];
+					fgets(line2, 5001, stdin);
+					if (strcmp(line2, "exit\n") == 0) {
+						printf("Server shutting down.\n");
+						for (int k = 0; k < FD_SETSIZE; k++) {
+							send(k, line2, strlen(line2) + 1, 0);
+						}
+						close(sockfd);
+						FD_CLR(sockfd, &sockets);
+						exit(0);
+					} else {
+						for (int k = 0; k < FD_SETSIZE; k++) {
+							send(k, line2, strlen(line2) + 1, 0);
+						}
+					}
+				} else if (i) {
+					char line[5000];
+					recv(i, line, 5000, 0);
+					printf("%s\n", line);
+					if (strcmp(line, "exit\n") == 0) {
+						printf("Server shutting down.\n");
+						for (int k = 0; k < FD_SETSIZE; k++) {
+							send(k, line, strlen(line) + 1, 0);
+						}
+						close(sockfd);
+						FD_CLR(sockfd, &sockets);
+						exit(0);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int check_port(char* port) {
+	int digits = 0;
+	for (int i = 0; i < sizeof(port); i++) {
+		if ((port[i] != '0') && !atoi(&port[i])) digits++;
+	}
+	if ((digits > 0 && digits < 6) && (atoi(port) > 0 && atoi(port) < 65536)) return 1;
+	else return 0;
+}
